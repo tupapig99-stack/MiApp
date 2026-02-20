70ffadf7ee0a660759261eca56940284d445f312
tupapig99 <tupapig99@gmail.com>
2026-02-20 05:41:26 -0500

    Activar layout con input

--- app/src/main/java/com/miapp/MainActivity.java
+++ app/src/main/java/com/miapp/MainActivity.java
@@ -2,16 +2,24 @@ package com.miapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        TextView text = new TextView(this);
-        text.setText("Hola Mundo 🚀");
-        text.setTextSize(24);
-        setContentView(text);
+        EditText editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(v -> {
+            String texto = editText.getText().toString();
+            textView.setText(texto);
+        });
     }
 }
